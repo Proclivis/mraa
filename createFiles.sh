d8e6d5d55d509ab5c1111a480f5c13b9ccb214f4
Michael Jones <mjones@linear.com>
2015-06-23 08:32:31 -0600

    Cleanup create Script

--- createFiles.sh
+++ createFiles.sh
@@ -1,7 +1,3 @@
-# c2hs --cppopts='-I/usr/include' --output='src/System/Hardware/I2C/Linux/I2CDev.hs' 'src/System/Hardware/I2C/Linux/chs/I2CDev.chs'
 c2hs --cppopts='-I/usr/include' --output='src/System/Hardware/Mraa/Mraa.hs' 'src/System/Hardware/Mraa/chs/Mraa.chs'
-# c2hs --cppopts='-Isrc/cbits' --include='src/cbits' --output='src/System/Hardware/I2C/Linux/SMBusDev2.hs' 'src/System/Hardware/I2C/Linux/chs/SMBusDev2.chs'
-# rm src/System/Hardware/I2C/Linux/*.h
-# rm src/System/Hardware/I2C/Linux/*.chi
 rm src/System/Hardware/Mraa/*.h
 rm src/System/Hardware/Mraa/*.chi
